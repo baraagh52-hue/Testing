6277f7ec6c9c709130ed531fbf3864c1291afbae
baraagh52 <baraagh52@gmail.com>
2025-09-02 20:05:27 +0000

    Feat: Add placeholder Weather and ActivityWatch services

---
+++ Ai_Assistant/Ai_Assistant/ActivityWatchService.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+
+namespace Ai_Assistant
+{
+    public class ActivityWatchService
+    {
+        public Task<string> GetTotalUnproductiveTimeAsync()
+        {
+            // Placeholder implementation
+            return Task.FromResult("a few minutes");
+        }
+    }
+}
---
+++ Ai_Assistant/Ai_Assistant/WeatherService.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+
+namespace Ai_Assistant
+{
+    public class WeatherService
+    {
+        public Task<string> GetWeatherAsync()
+        {
+            // Placeholder implementation
+            return Task.FromResult("sunny with a chance of bugs");
+        }
+    }
+}
